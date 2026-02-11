2dc66f8f39950f782bd9aa52f8e4f3bbeed3a213
cesarZaabra <cesaralejandroantolinez@gmail.com>
2026-02-11 17:28:11 -0500

    arreglar rutas

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Livewire\CreateCustomer;
+use App\Http\Livewire\CreatePaymentLink;
+use App\Http\Livewire\CreatePlan;
+use App\Http\Livewire\CreateSubscription;
+use App\Http\Livewire\ManageCards;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +21,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/customers/create', CreateCustomer::class)->name('customers.create');
+Route::get('/plans/create', CreatePlan::class)->name('plans.create');
+Route::get('/subscriptions/create', CreateSubscription::class)->name('subscriptions.create');
+Route::get('/payment-links/create', CreatePaymentLink::class)->name('payment-links.create');
+Route::get('/cards', ManageCards::class)->name('cards.index');
