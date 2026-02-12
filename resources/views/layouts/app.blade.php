dae9b20a678ca7776e26db2822fbc09951e6e9c4
cesarZaabra <cesaralejandroantolinez@gmail.com>
2026-02-12 16:55:12 -0500

    arreglar frontend

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,8 @@
         @livewireStyles
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link href="{{ asset('build/assets/app-BF9iHclu.css') }}" rel="stylesheet">
+        <script src="{{ asset('build/assets/app-BF9iHclu.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased">
         <x-jet-banner />
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,7 +11,8 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link href="{{ asset('build/assets/app-BF9iHclu.css') }}" rel="stylesheet">
+        <script src="{{ asset('build/assets/app-BF9iHclu.js') }}" defer></script>
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
